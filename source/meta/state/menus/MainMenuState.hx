662596d927870185752a8381e876e0631bdc7b99
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-10-24 13:36:58 -0300

    Update MainMenuState.hx

--- source/meta/state/menus/MainMenuState.hx
+++ source/meta/state/menus/MainMenuState.hx
@@ -74,8 +74,7 @@ class MainMenuState extends MusicBeatState
 		super.create();
 
 		ForeverTools.resetMenuMusic();
-		Discord.changePresence('MAIN MENU', 'Main Menu');
-
+		
 		// POKEMON YELLOW LOL
 		backdrop = new FlxBackdrop(Paths.image('menus/menu/pokemon_yellow_noise'), 1, 1, true, true, 1, 1);
 		add(backdrop);
@@ -128,6 +127,10 @@ class MainMenuState extends MusicBeatState
 		}
 		add(textGroup);
 		add(lockGroup);
+	
+	#if android
+	addVirtualPad(UP_DOWN, A_B);
+	#end
 	}
 
 	public var curSelection:Int = 0;
